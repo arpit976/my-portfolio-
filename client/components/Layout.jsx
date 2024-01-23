463a9029618d36805793e4d2165d7183776d6ade
Arpit Savaliya <arpitsavaliya976@gmail.com>
2024-01-23 11:24:56 -0500

    About me page

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom';
 export default function Layout() {
   return (
     <>
-    <div class="header">
-      <h1>My Portfolio</h1>
-      <p>Hi everyone welcome to the site.....</p>
-    </div>
-      <div>
-        <nav>
-          <Link to="/">Home Page</Link> | <Link to="/about">About Me</Link> | <Link to="/services">Services</Link> | <Link to="/project">My Project</Link> | <Link to="/contact">Contact ME</Link>
-        </nav>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <header >
+        
+      <div class="header">
+        <h1>My Portfolio</h1>
+        <p>Hi, Welcome to the site.....</p>
+      </div>
+          
+      <div class="topnav">
+        <a href="/">Home</a>
+        <a href="/about">About Me</a>
+        <a href="/services">Services</a>  
+        <a href="/project">Project</a>
+        <a href="/contact" class="split">Contact ME</a>
       </div>
       <hr />      
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </header>
     </>
   );
 }
--- client/src/about.jsx
+++ client/src/about.jsx
@@ -1,12 +1,36 @@
 export default function About() {
       return (
-        <>
-          <div class="about-section">
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
-</div>
-
+        <>     
+          <div class="container">
+            <div class="about-section">
+              <h1>About Me</h1>
+            </div>
+              <div >
+                <h2>Introduction</h2>
+                <p>
+                  I am a 23 year old finnish Game developer with science and art background. I love to spend my free time making and playing new game. I love scifi and fantasy literature and
+                  series. 
+                </p>
+              </div>
+              <div >
+                <h3>My Skills</h3>
+                <ul id="skill-list">
+                  <li>Game Development</li>
+                  <li>Clean Programming</li>
+                  <li>OOP</li>						
+                  <li>c#/c++/java</li>
+                </ul>
+              </div>
+              <div >
+                <h3>More About Me</h3>
+                <p>
+                  I have studied Information Technology but have always been interested in Game development. I was 16 years old when I made my first Game with unity. While I was studying in the university I took computer science  as my major subject and was introduced to programming. During my studies I was an active member in student associations and became responsible for all kinds of tasks and projects including being a Game Developer. That and my previous interest in game development created a spark to turn my hobby into a career. I took my first steps towards my new career in January 2022.
+                </p>
+                <p>
+                  I'm highly energetic and motivated person and I love to work as a member of a team. I especially enjoy working with changing projects and taking them forward with rapid pace but high precision. I am talkative and sociable but also known for getting "in the zone" while concentrating on detailed work like programming or animating.
+                </p>
+              </div>
+            </div>
         </>
       );
     }
