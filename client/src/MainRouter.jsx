7b400c9284d57e30e738e732236200933ef9d0cf
Arpit Savaliya <arpitsavaliya976@gmail.com>
2024-01-25 11:13:28 -0500

    debugging

--- client/src/MainRouter.jsx
+++ client/src/MainRouter.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from '../components/Home'
+import Home from './../components/Home.jsx'
 // import About from './src/about'
 
-import About from './../src/about'
-import Contact from './../src/contact'
-import Education from './../src/services'
-import Project from './../src/project'
-import Layout from './../components/Layout'
+import About from './../src/about.jsx'
+import Contact from './../src/contact.jsx'
+import Education from './../src/services.jsx'
+import Project from './../src/project.jsx'
+import Layout from './../components/Layout.jsx'
 const MainRouter = () => {
         return (<div>
         <Layout/>
